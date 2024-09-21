3812cef20824ccd521cb7ca2b209421bb3eb1126
Marcos Santos <marcos.diego.ns@outlook.com>
2024-09-21 14:02:31 -0500

    refactor: wrap Product component with Link for better routing
    Previously, the Product component was responsible for handling the routing logic. This change refactors the code to use the Next.js Link component to handle routing, improving readability and adherence to Next.js conventions.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -27,18 +28,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product
-            key={product.id}
-            href={`/product/${product.id}`}
-            className="keen-slider__slide"
-          >
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
