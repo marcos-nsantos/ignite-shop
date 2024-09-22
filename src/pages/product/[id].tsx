e5b850bb273ec05f09fd95723dced03a026a0ee8
Marcos Santos <marcos.diego.ns@outlook.com>
2024-09-22 07:38:41 -0500

    feat: add product page styling and initial layout
    Introduce styles for the product page, including containers for main layout, image, and product details. Update the product page to utilize these styles and display a basic product structure with title, price, description, and purchase button.

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,32 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 
 export default function Index() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  console.log(query);
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+          aliquid rerum exercitationem facere a molestiae ut sed velit non
+          mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed
+          laboriosam tempora?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
