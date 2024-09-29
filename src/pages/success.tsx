6c760c208f5af5727f4a0730dde6164efde0ace5
Marcos Santos <marcos.diego.ns@outlook.com>
2024-09-29 06:43:11 -0500

    feat: add dynamic session data to success page
    Implemented server-side logic to fetch and display dynamic customer and product data on the success page based on session ID. Updated checkout API to pass the session ID to the success page.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,20 +1,64 @@
 import { ImageContainer, SuccessContainer } from "@/src/styles/pages/success";
 import Link from "next/link";
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+import { stripe } from "@/src/lib/stripe";
+import Stripe from "stripe";
 
-export default function Success() {
+interface SuccessProps {
+  costumerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ costumerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{costumerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+  let session;
+
+  try {
+    session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ["line_items", "line_items.data.price.product"],
+    });
+  } catch (error) {
+    console.error("Error retrieving session:", error);
+    return {
+      notFound: true,
+    };
+  }
+
+  const customerName = session.customer_details?.name ?? "Unknown Customer";
+
+  const product = session.line_items?.data?.[0]?.price
+    ?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product?.name ?? "Unknown Product",
+        imageUrl: product?.images?.[0] ?? "default_image_url",
+      },
+    },
+  };
+};
